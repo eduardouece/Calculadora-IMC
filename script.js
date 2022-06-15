aa00f5af55088021f00ea437026cffccb802ec13
Eduardo Monteiro <103010827+eduardouece@users.noreply.github.com>
2022-06-15 15:32:11 +0000

    Correção de erros

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
-
-const calcular = document.getElementById('calcular');
-
+var calcular = document.getElementById('calcular');
 
 function imc (){
-const nome = document.getElementById('nome').value;
-const altura = document.getElementById('altura').value;
-const peso = document.getElementById('peso').value;
-const resultado = document.getElementById('resultado');
+  let nome = document.getElementById('nome').value;
+  let altura = document.getElementById('altura').value;
+  let peso = document.getElementById('peso').value;
+  let resultado = document.getElementById('resultado');
+   
+    if (nome !== '' && altura !== '' && peso !== ''){
+      let valorIMC = (peso / (altura * altura)).toFixed(1);
+      let classificacao =  document.getElementById("resultado");
 
-if (nome !== '' && altura !== '' && peso !== ''){
-  const valorIMC = (peso / (altura * altura)).toFixed(1)
+        if (valorIMC < 18.5){
+         classificacao.innerHTML = "abaixo do peso.";
+        } else if (valorIMC < 25){
+          classificacao.innerHTML = 'com peso ideal. Parabéns!!';
+        }else if (valorIMC < 30) {
+          classificacao.innerHTML = 'levemente acima do peso';
+        }else if (valorIMC < 35) {
+          classificacao.innerHTML = 'obesidade grau I.';
+        }else if (valorIMC < 40) {
+          classificacao.innerHTML = 'com obesidade grau II'
+        } else {
+          classificacao.innerHTML = 'com obesidade grau III. Cuidado!!!'
+        }
 
-  let classificacao = '';
-
-  if (valorIMC < 18.5)
-    {classificacao = 'abaixo do peso.';}
-  else if (valorIMC < 25){
-    {classificacao = 'com peso ideal. Parabéns!!';}
-  else if (valorIMC < 30) {
-    classificacao = 'levemente acima do peso';
-  }
-  else if (valorIMC < 35) {
-    classificacao = 'obesidade grau I.';
-  }
-  else if (valorIMC < 40) {
-    classificacao = 'com obesidade grau II'
-  }
-  else {
-    classificacao = 'com obesidade grau III. Cuidado!!!'
-  }
-    
-  
-  
-resultado.textContent = '${nome} seu IMC é ${valorIMC} e você está ${classificacao}. ';
-  
-else {
-  resultado.textContent = 'Preencha todos os campos!!!'
-}}
-  
-
-
+    }else {
+      resultado.textContent = 'Preencha todos os campos!!!'
+    }
+}
 calcular.addEventListener('click', imc);
+
+  
+
+
