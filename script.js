9a3ab351bbfc8019788aca0c09b88384a13b6929
Eduardo Monteiro <103010827+eduardouece@users.noreply.github.com>
2022-06-15 14:35:00 +0000

    Atualização HTML e JS

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const calcular = document.getElementById('calcular');
 
 
-function imc ()
+function imc (){
 const nome = document.getElementById('nome').value;
 const altura = document.getElementById('altura').value;
 const peso = document.getElementById('peso').value;
@@ -36,7 +36,7 @@ resultado.textContent = '${nome} seu IMC é ${valorIMC} e você está ${classifi
   
 else {
   resultado.textContent = 'Preencha todos os campos!!!'
-}
+}}
   
 
 
